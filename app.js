8c91184f874e53b0c78f55ca6365575663af9c0b
Sander Hestvik <sander.hestvik@gmail.com>
2019-12-13 13:05:25 +0100

    add necessary files to make linker chome extension

---
+++ app.js
@@ -0,0 +1,15 @@
+const courseName = $("#course-details").attr("data-coursecode");
+const courseTitleJQueryElement = $("#course-details").children("h1").first();
+
+const title = courseTitleJQueryElement.html();
+courseTitleJQueryElement.html("<a target=\"_blank\" href=\"https://www.grades.no/course/" + courseName + "/\">" + title + "</a>");
+
+
+/*
+const paragraphElement = $("#assessment-toggler > div.content-assessment > p").first();
+let paragraphText = paragraphElement.html();
+const gradesApiUrl = "https://grades.no/api/courses/" + courseName + "/grades";
+*/
+
+
+console.log("Script ran");
